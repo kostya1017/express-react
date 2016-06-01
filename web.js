784a2ffa99fee5f2e3e4999ef2e75d2ee88b514e
kostyaboreski <kostyaboreski1017>
2016-06-01 05:55:14 +0300

    express test

---
+++ web.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello! This is kostya from Heroku. ');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
